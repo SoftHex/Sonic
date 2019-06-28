03b3ed5f3a991c0821c27b050e659f67ad78fb5f
Carlos Henrique <tech.carloshenrique@gmail.com>
2019-06-28 01:15:36 -0300

    Adjust Commit

--- app/src/main/java/com/softhex/sonic/MainActivity.java
+++ app/src/main/java/com/softhex/sonic/MainActivity.java
@@ -35,8 +35,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if(Build.VERSION.SDK_INT>=21){
-            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE|View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         }
 
         DBC = new sonicDatabaseCRUD(getBaseContext());
